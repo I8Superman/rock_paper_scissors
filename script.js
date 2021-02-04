7956c23f5038495662a3bf89421f4d9d0023b1f9
Simon Andersen <simo414u@stud.kea.dk>
2021-02-04 16:55:13 +0100

    Js rough structure in place

---
+++ script.js
@@ -0,0 +1,30 @@
+// Rock, paper, scissors script
+
+let player_points = 0;
+let ai_points = 0;
+let round_nr = 0;
+
+function startGame() {
+  // Initiates the whole thing
+  // Add eventlisteners to figures
+  // Player can click either rock, paper or scissors
+}
+
+function figureClicked() {
+  // AI's choise is randomly generated
+  // Player and AI's choises are compared - points are awarded if there is a win and newRound() is initiated
+  // Result of round is shown (You win/loose this round! It's a draw!)
+  // If player or AI has 2 points, initiate gameEnd()
+}
+
+function newRound() {
+  // Eventlisteners are reset
+  // Info on status conveyed to player
+  // Initiate startGame (right??)
+  //
+}
+
+function endGame() {
+  // If player has 2 points - YOU WIN!
+  // If AI has 2 points - YOU LOSE!
+}
