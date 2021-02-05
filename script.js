9ba731561da5068aca147c89714b60e292e2d5cd
Simon Andersen <simo414u@stud.kea.dk>
2021-02-05 18:46:51 +0100

    class scissors spelling eror fixed

--- script.js
+++ script.js
@@ -20,31 +20,15 @@ function figureClicked() {
   console.log("Figure clicked");
   // this gets the element that was clicked
   let figure = this;
-  console.log(this);
+  // console.log(this);
   // Set player_choice variable to be equal to class name
   player_choice = this.getAttribute("class");
   console.log("You chose " + player_choice);
-  // Generate random number
-  ai_random = Math.floor(Math.random() * 3) + 1;
-  //console.log("AI chose " + ai_random);
-  // Assign string to random number and set ai_choice variable to that string
-  if (ai_random == 1) {
-    ai_choice = "rock";
-  }
-  if (ai_random == 2) {
-    ai_choice = "paper";
-  }
-  if (ai_random == 3) {
-    ai_choice = "scissor";
-  }
-  console.log("AI chose " + ai_choice);
-
+  // Add shake animation
   document.querySelectorAll(".player").forEach((elem) => {
     elem.classList.add("shake");
   });
-  document
-    .querySelector("#player1")
-    .addEventListener("animationend", checkResult);
+  document.querySelector("#player1").addEventListener("animationend", checkResult);
   // AI's choise is randomly generated
   // Player and AI's choises are compared - points are awarded if there is a win and newRound() is initiated
   // Result of round is shown (You win/loose this round! It's a draw!)
@@ -56,11 +40,23 @@ function checkResult() {
   document.querySelectorAll(".player").forEach((elem) => {
     elem.classList.remove("shake");
   });
-  document
-    .querySelector("#player1")
-    .removeEventListener("animationend", checkResult);
-
+  // Generate random number
+  ai_random = Math.floor(Math.random() * 3) + 1;
+  //console.log("AI chose " + ai_random);
+  // Assign string to random number and set ai_choice variable to that string
+  if (ai_random == 1) {
+    ai_choice = "rock";
+  }
+  if (ai_random == 2) {
+    ai_choice = "paper";
+  }
+  if (ai_random == 3) {
+    ai_choice = "scissors";
+  }
+  console.log("AI chose " + ai_choice);
+  document.querySelector("#player1").removeEventListener("animationend", checkResult);
   document.querySelector("#player1").classList.add(player_choice);
+  document.querySelector("#player2").classList.add(ai_choice);
 }
 
 function newRound() {
